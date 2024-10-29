a46783ba43ab0386302e44dc3ac3380e35a7a155
Bhavesh kodam <110908684+bhxvesh21@users.noreply.github.com>
2024-10-29 13:19:27 +0530

    Add files via upload

---
+++ src/components/FilterDropdown.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './FilterDropdown.css';
+
+function FilterDropdown({ filter, setFilter }) {
+  return (
+    <div className="filter-dropdown">
+      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+        <option value="">All Types</option>
+        <option value="Full-Time">Full-Time</option>
+        <option value="Part-Time">Part-Time</option>
+        <option value="Remote">Remote</option>
+        <option value="Hybrid">Hybrid</option>
+        <option value="Work-from home">work-from home</option>
+      </select>
+    </div>
+  );
+}
+
+export default FilterDropdown;
